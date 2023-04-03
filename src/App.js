39ca2787e6bff1ced28880d9113bb6f0af24db0b
Aryawardhan <aagarwalai@connect.ust.hk>
2023-04-03 18:18:28 +0800

    staged commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const TodoItem = ({ todo, toggleTodo }) => {
+  return (
+    <li
+      style={{
+        textDecoration: todo.completed ? 'line-through' : 'none',
+        cursor: 'pointer',
+      }}
+      onClick={() => toggleTodo(todo.id)}
+    >
+      {todo.text}
+    </li>
+  );
+};
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [filter, setFilter] = useState('all');
+
+  useEffect(() => {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (!newTodo.trim()) return;
+    setTodos([
+      ...todos,
+      {
+        id: Date.now(),
+        text: newTodo,
+        completed: false,
+      },
+    ]);
+    setNewTodo('');
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const filteredTodos = todos.filter(
+    (todo) => filter === 'all' || (filter === 'done' && todo.completed) || (filter === 'notdone' && !todo.completed)
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add new todo"
+        />
+        <button type="submit">Add</button>
+      </form>
+      <div>
+        <button onClick={() => setFilter('all')}>All</button>
+        <button onClick={() => setFilter('done')}>Completed</button>
+        <button onClick={() => setFilter('notdone')}>Not Completed</button>
+      </div>
+      <ul style={{ maxHeight: '200px', overflowY: 'scroll', padding: 0 }}>
+        {filteredTodos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
